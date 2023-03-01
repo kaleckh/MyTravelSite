062264c89218a641dc5304ac942195640545a8a7
kaleckh <69163050+kaleckh@users.noreply.github.com>
2023-03-01 16:42:48 -0700

    loading on sever

--- src/Components/Person.js
+++ src/Components/Person.js
@@ -12,25 +12,27 @@ import { useLocation } from "react-router-dom";
 import { auth } from "./Firebase";
 import DateRangePicker from "@wojtekmaj/react-daterange-picker";
 import { Search } from "./Media/Search";
-const {REACT_APP_URL} = process.env
+import { Audio } from "react-loader-spinner";
+const { REACT_APP_URL } = process.env;
 function Person() {
   const Navigate = useNavigate();
   const [tripLocation, setTripLocation] = useState("");
   const [people, setPeople] = useState([]);
   const [tripDates, setTripDates] = useState("");
-  const [isSettingLocation, setIsSettingLocation] = useState(false);
-  const [isSettingDate, setIsSettingDate] = useState(false);
+
   const [isAdding, setIsAdding] = useState(false);
   const [myTrips, setMyTrips] = useState([]);
   const [myId, setMyId] = useState("");
   const [isAddingTrip, setIsAddingTrip] = useState(false);
   const [value, onChange] = useState([new Date(), new Date()]);
-  const [tripCity, setTripCity] = useState("");
+
   const [tripState, setTripState] = useState("");
   const [trips, setTrips] = useState([]);
   const [description, setDescription] = useState("");
   const { search } = useLocation();
   const params = new URLSearchParams(search);
+  const [isLoading, setIsLoading] = useState(true);
+  const [data, setData] = useState([]);
 
   const localEmail = localStorage.getItem("userEmail");
 
@@ -84,7 +86,7 @@ function Person() {
       method: "get",
       url: `${REACT_APP_URL}/trips`,
     }).then((res) => {
-      setTrips(res.data)
+      setTrips(res.data);
     });
   }, []);
 
@@ -96,6 +98,15 @@ function Person() {
       setMyId(res.data[0].id);
     });
   }, []);
+  useEffect(() => {
+    axios
+      .get(`${REACT_APP_URL}/person/${localStorage.getItem("userEmail")}`)
+      .then((response) => {
+        setData(response.data);
+        setIsLoading(false);
+      });
+  }, []);
+
   console.log(trips, "these are my trips");
 
   return (
@@ -103,7 +114,6 @@ function Person() {
       <div className={styles.wholeScreen}>
         <Header
           home={"Home"}
-          
           myTrips={"My Trips"}
           myProfile={"My Profile"}
           out={"Logout"}
@@ -112,98 +122,118 @@ function Person() {
             setIsAddingTrip(!isAddingTrip);
           }}
         />
-        {isAddingTrip ? (
-          <div className={styles.center}>
-            <div className={`${styles.createTripContainer} ${styles.blur}`}>
-              <div className={styles.halfTripContainer}>
-                <div className={styles.blueBoxWords}>
-                  Start your journey with us
-                </div>
-                <div className={styles.smallBlueBoxWords}>
-                  Join our community of thousands
-                </div>
-                <div>A quote would go here</div>
-              </div>
-
-              <div className={styles.otherHalfContainer}>
-                <div className={styles.eightyPercent}>
-                  <div className={styles.xContainer}>
-                    <div
-                      className={styles.x}
-                      onClick={() => {
-                        setIsAddingTrip(false);
-                      }}
-                    >
-                      x
-                    </div>
-                  </div>
-
-                  <div className={styles.createTripInputContainer}>
-                    <div className={styles.half}>
-                      <div>city</div>
-                      <input
-                        className={styles.createTripInput}
-                        placeholder="City"
-                        onChange={(event) => {
-                          setTripLocation(event.target.value);
-                        }}
-                        type="text"
-                      />
-                    </div>
-                    <div className={styles.half}>
-                      <div>State/Country</div>
-                      <input
-                        onChange={(event) => {
-                          setTripState(event.target.value);
-                        }}
-                        placeholder="State/Country"
-                        type="text"
-                        className={styles.createTripInput}
-                      />
-                    </div>
-                  </div>
-
-                  <div className={styles.datePickerContainer}>
-                    <DateRangePicker
-                      onChange={onChange}
-                      value={value}
-                      className={styles.datePicker}
-                    />
-                  </div>
-                  <textarea
-                    onChange={(event) => {
-                      setDescription(event.target.value);
-                    }}
-                    className={styles.tripInfo}
-                    placeholder="Description of what you guys will do"
-                  />
-                  <div className={styles.nextContainer}>
-                    <button
-                      className={styles.createButton}
-                      onClick={() => {
-                        handleSubmit().then(() => {
-                          setIsAdding(false);
-                        });
-                      }}
-                    >
-                      Next!
-                    </button>
-                  </div>
-                </div>
-              </div>
-            </div>
+        {isLoading ? (
+          <div className={styles.audio}>
+            {" "}
+            <Audio
+              height="80"
+              width="80"
+              radius="9"
+              color="white"
+              ariaLabel="loading"
+              wrapperStyle
+              wrapperClass
+            />
+            <div className={styles.free}>My servers are free</div>
+            <div className={styles.smallfree}>Give it a sec haha</div>
           </div>
         ) : (
-          <div className={styles.searchContainer}>
-            <div className={styles.inputSizer}>
-              <div className={styles.title}>Open Trips</div>
-              <div className={styles.container}>
-                <SearchBar trips={trips} />
-                <div></div>
+          <>
+            {isAddingTrip ? (
+              <div className={styles.center}>
+                <div className={`${styles.createTripContainer} ${styles.blur}`}>
+                  <div className={styles.halfTripContainer}>
+                    <div className={styles.blueBoxWords}>
+                      Start your journey with us
+                    </div>
+                    <div className={styles.smallBlueBoxWords}>
+                      Join our community of thousands
+                    </div>
+                    <div>A quote would go here</div>
+                  </div>
+
+                  <div className={styles.otherHalfContainer}>
+                    <div className={styles.eightyPercent}>
+                      <div className={styles.xContainer}>
+                        <div
+                          className={styles.x}
+                          onClick={() => {
+                            setIsAddingTrip(false);
+                          }}
+                        >
+                          x
+                        </div>
+                      </div>
+
+                      <div className={styles.createTripInputContainer}>
+                        <div className={styles.half}>
+                          <div>city</div>
+                          <input
+                            className={styles.createTripInput}
+                            placeholder="City"
+                            onChange={(event) => {
+                              setTripLocation(event.target.value);
+                            }}
+                            type="text"
+                          />
+                        </div>
+                        <div className={styles.half}>
+                          <div>State/Country</div>
+                          <input
+                            onChange={(event) => {
+                              setTripState(event.target.value);
+                            }}
+                            placeholder="State/Country"
+                            type="text"
+                            className={styles.createTripInput}
+                          />
+                        </div>
+                      </div>
+
+                      <div className={styles.datePickerContainer}>
+                        <DateRangePicker
+                          onChange={onChange}
+                          value={value}
+                          className={styles.datePicker}
+                        />
+                      </div>
+                      <textarea
+                        onChange={(event) => {
+                          setDescription(event.target.value);
+                        }}
+                        className={styles.tripInfo}
+                        placeholder="Description of what you guys will do"
+                      />
+                      <div className={styles.nextContainer}>
+                        <button
+                          className={styles.createButton}
+                          onClick={() => {
+                            handleSubmit().then(() => {
+                              setIsAdding(false);
+                            });
+                          }}
+                        >
+                          Next!
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
+            ) : (
+              <div className={styles.searchContainer}>
+                <div className={styles.inputSizer}>
+                  <div className={styles.title}>Open Trips</div>
+                  <div className={styles.container}>
+                    <SearchBar trips={trips} />
+                    <div></div>
+                  </div>
+                </div>
+              </div>
+            )}
+          </>
         )}
+
         <div className={styles.downContainer}>
           <div className={styles.bottomText}>Huh?</div>
           <Arrow />

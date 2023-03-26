6d764e00595d90ae67aa7965b6ccaaa0b9a23105
kaleckh <69163050+kaleckh@users.noreply.github.com>
2023-03-26 11:42:02 -0600

    make loading better

--- src/server/index.js
+++ src/server/index.js
@@ -1,60 +1,59 @@
-require('dotenv').config();
-const express = require('express');
-const massive = require('massive');
-const bodyParser = require('body-parser');
+require("dotenv").config();
+const express = require("express");
+const massive = require("massive");
+const bodyParser = require("body-parser");
 var {
-	getpeople,
-	edittrip,
-	newperson,
-	editdetails,
-	deleteperson,
-	getPerson,
-	getPersonTrips,
-	newTrip,
+  getpeople,
+  edittrip,
+  newperson,
+  editdetails,
+  deleteperson,
+  getPerson,
+  getPersonTrips,
+  newTrip,
   deletetrip,
   getTrip,
   getTrips,
   getTripGroup,
   newTripGroup,
-  deleteTripGroup
-  
-} = require('./controller');
+  deleteTripGroup,
+} = require("./controller");
 const app = express();
 app.use(express.json());
 const { PORT = 3001 } = process.env;
-const cors = require('cors');
+const cors = require("cors");
 app.use(cors());
 app.use(bodyParser());
-const dotenv = require('dotenv');
-require('dotenv').config();
-const CONNECTION_STRING = 'postgresql://postgres:jDSjkH8jyPRQmSKJ9MBh@containers-us-west-22.railway.app:6635/railway';
+const dotenv = require("dotenv");
+require("dotenv").config();
+const CONNECTION_STRING =
+  "postgresql://postgres:jDSjkH8jyPRQmSKJ9MBh@containers-us-west-22.railway.app:6635/railway";
 massive({
-	connectionString: CONNECTION_STRING,
-	ssl: { rejectUnauthorized: false }
+  connectionString: CONNECTION_STRING,
+  ssl: { rejectUnauthorized: false },
 })
-	.then((dbInstance) => {
-		app.set('db', dbInstance);
-	})
-	.catch((err) => console.log(err));
+  .then((dbInstance) => {
+    app.set("db", dbInstance);
+    app.get("/people", getpeople);
+    app.get("/heartbeat", (req, res) => {
+      res.json({
+        is: "alive",
+      });
+    });
+    app.get("/person/:email", getPerson);
+    app.get("/personTrips/:id", getPersonTrips);
+    app.get("/trips", getTrips);
+    app.get("/trip/:id", getTrip);
+    app.get("/tripgroup/:id", getTripGroup);
+    app.post("/newperson", newperson);
+    app.post("/newtrip", newTrip);
+    app.post("/newtripgroup", newTripGroup);
+    app.put("/editperson/:id", editdetails);
+    app.put("/edittrip/:id", edittrip);
+    app.delete("/deleteperson/:id", deleteperson);
+    app.delete("/deletetrip/:id", deletetrip);
+    app.delete("/deletetripgroup/:id", deleteTripGroup);
 
-app.get('/people', getpeople);
-app.get('/heartbeat', (req,res) => {
-	res.json({
-		is: "alive"
-	})
-})
-app.get('/person/:email', getPerson);
-app.get('/personTrips/:id', getPersonTrips);
-app.get('/trips', getTrips);
-app.get('/trip/:id', getTrip)
-app.get('/tripgroup/:id', getTripGroup)
-app.post('/newperson', newperson);
-app.post('/newtrip', newTrip);
-app.post('/newtripgroup', newTripGroup);
-app.put('/editperson/:id', editdetails); 
-app.put('/edittrip/:id', edittrip); 
-app.delete('/deleteperson/:id', deleteperson);
-app.delete('/deletetrip/:id', deletetrip);
-app.delete('/deletetripgroup/:id', deleteTripGroup);
-
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  })
+  .catch((err) => console.log(err));
